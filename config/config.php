6888783d7a6e30cb75e4cd19f74620edd56f5461
Fherhad Aditya <107832607+peyadbot@users.noreply.github.com>
2024-08-19 13:53:10 +0800

    Add .env file for configuration and load environment variables with phpdotenv

--- config/config.php
+++ config/config.php
@@ -1,10 +1,16 @@
 <!-- Database configuration -->
 
 <?php
-$host = 'localhost';
-$dbname = 'statistic_archery';
-$username = 'root';
-$password = '';
+// composer require vlucas/phpdotenv
+require_once __DIR__ . '/../vendor/autoload.php';
+
+$dotenv = Dotenv\Dotenv::createImmutable(__DIR__.'/..');
+$dotenv->load();
+
+$host = $_ENV['DB_HOST'];
+$dbname = $_ENV['DB_NAME'];
+$username = $_ENV['DB_USER'];
+$password = $_ENV['DB_PASS'];
 
 try {
     $pdo = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8", $username, $password);
